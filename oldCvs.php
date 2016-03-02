b4625990f8b70ac05e94c310f699d0357dbb4a2c
kokojack <kokojackeve@gmail.com>
2016-03-02 01:05:17 +0100

    Initialisation du PortFolio

---
+++ oldCvs.php
@@ -0,0 +1,192 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Curriculum Vitae 2015</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="afficheCss.css">
+    </head>
+
+    <body>
+         <a name="haut"><marquee><h2> CURRICULUM VITAE </h2></marquee></a>
+         <div>
+             <img src="Copie de 20140905_110453.jpg" class="img">
+             <img id ="messi" src="http://netdna.webdesignerdepot.com/uploads7/50-inspiring-animated-gifs/006.gif" width="300" height="200">
+             <img id="leo" src="http://graphics8.nytimes.com/images/2013/05/21/technology/21-baby/21-baby-custom1.gif" width="300" height="200">
+        <address name="adresse" class="adresse" id= "nonrelief">
+           Jackeve Koko Momo Kenfack<br>
+           20 ans<br>
+           1, avenue francis de pressense<br>
+           93000 Aubervilliers<br>
+           0782706107<br>
+           kokojackeve@hotmail.com<br>
+        </address>
+    </div>
+         <div>
+        
+        <table cols = 10 rows = 21>
+            
+            <caption> Curriculum Vitae 2015</caption>
+            
+            <tr>
+                
+                <th colspan=10>OFFRE DE SERVICES INFORMATIQUES</th>
+                
+            </tr>    
+            
+            <tr>
+                <th colspan=10 name="formations"> FORMATIONS </th>
+            </tr>
+            
+            <tr>
+                <td class="annee">2014 - 2015</td>
+                <td class="intitule">BTS SIO(Services informatiques aux organismes) <ul><li><address class="adresse">Itic Paris-20ème</address> <hr><hr size="10" color="#fff"></li></ul></td>
+            </tr>
+            
+            <tr>
+                <td class="annee">2013 - 2014</td>
+                <td class="intitule">Mathématiques-informatique <ul><li><address class="adresse"> Université de Dschang (Cameroun)</address><hr><hr size="10" color="#fff"></li></ul></td>
+            </tr>
+            
+             <tr>
+                <td class="annee">2012 - 2013</td>
+                <td class="intitule">Mathématiques-informatique <ul><li><address class="adresse"> Université de Dschang (Cameroun)</address><hr><hr size="10" color="#fff"></li></ul></td>
+            </tr>
+            
+             <tr>
+                <td class="annee">2011 - 2012</td>
+                <td class="intitule">Baccalauréat S <ul><li><address class="adresse"> Lycée de Biyem-Assi Yaoundé(Cameroun)</address><hr><hr size="10" color="#fff"></li></ul></td>
+            </tr>
+            
+            <tr>
+                <th colspan=10 name="experiences"> EXPERIENCE PROFESSIONELLE</th>
+            </tr>
+            
+            <tr>
+                 <td class="annee">01-05-2015 - 31-08-2015</td>
+                <td class="intitule">Stage technicien de support informatique dans l'entreprise Solvay <ul><li><address class="adresse"> 40 Rue la Haie coq 93300 Aubervilliers </address><hr><hr size="10" color="#fff"></li></ul></td>
+            </tr>
+            
+            <tr>
+                          <td class="annee"> 10/2014 - 31/03/2015 </td>
+                <td class="intitule">ASH (Agent de Services Hospitaliers) en maison de retraite de Constance Mazier,<ul><li><address class="adresse"> 4 Rue Hemet 93300 Aubervilliers</address><hr><hr size="10" color="#fff"></li></ul></td>
+            </tr>
+            
+             <tr>
+                 <th colspan=10 name="competences">DOMAINE DE COMPETENCES</th>
+            </tr>
+            
+            
+            <tr>
+                <td class="annee">Systèmes d'exploitation / Operating system</td>
+                <td class="intitule">
+                    <ol type = 1>
+                    <li>Windows7 
+                    <li>Windows8 
+                    <li>Debian(Linux, Ubuntu)
+                    <li>Windows server 2008</li>
+                    <hr size="10" color ="#fff">
+                    </ol>
+                    </td>
+            </tr>
+            
+            <tr>
+                <td class="annee">Développement/Programmation</td>
+                <td>
+                    <ol type = 1>
+                        <li  class="intitule">Csharp (C#):
+                            <ul type = square>
+                                <li class="definition">Visual Studio</li>
+                                <li class="definition">Mono develop</li>
+                            </ul>
+                        </li>
+                        <br>
+                        <li  class="intitule">Web
+                            <ul type = square>
+                                <dl><li class="sous_intitule">Html:<dd class="definition">Hypertext Markup Language</dd></li>
+                                <li  class="sous_intitule"><dt>CSS:<dd class="definition">Cascading Style Sheet</dd></li>
+                                <li  class="sous_intitule"><dt>JavaScript:<dd class="definition">Langage interprété</dd></li></dl>
+                            </ul>
+                        </li>
+                        <li  class="intitule">Base de données
+                            <ul type ="square">
+                               <dl>
+                                   <li   class="sous_intitule">Sql:<dd class="definition">Structured Query Language</dd></li>
+                               <li   class="sous_intitule"><dt>Analyse:<dd class="definition">DDD, DF, MCD </dd></li>
+                               </dl>
+                           </ul>
+                        </li>
+                       
+                        <li class="intitule">Autres Langages
+                            <ul type = "square">
+                                <dl><li  class="sous_intitule">Java:<dd class="definition">Programmation Objet</dd></li>
+                                <dt><li  class="sous_intitule">Pascal: <dd class="definition">Notions d'algorithmie</dd></li>
+                                </dl>
+                            </ul>
+                        </li>
+                        <hr size="10" color ="#fff">
+                    </ol>    
+                </td>
+            </tr>
+            
+            <tr>
+                
+                <td class="annee">Bureautique</td>
+                <td class="intitule">
+                    <ul type = "square">
+                        <li>Google Drive</li>
+                        <li>Wordpress</li>
+                        <li>Word</li>
+                        <li>Power-point</li>
+                        <li>Suite libre Office</li>
+                    </ul>
+                </td>
+            </tr>
+            
+            <tr>
+                <th colspan=10 name="interets"> CENTRES D'INTERET</th>
+            </tr>
+            
+            <tr>
+                
+                <td class="annee">Sport</td>
+                <td class="intitule">
+                    <ul type = "square">
+                    <li>Football</li>
+                        <li>Tennis</li>
+                    </ul>
+                </td>
+            </tr>
+            
+            <tr>
+                
+                <td class="annee">Culture</td>
+                <td class="intitule">
+                    <ul type = "square">
+                        <li>Actualités Politiques</li>
+                        <li>Lecture (actualités, romans historiques, ...)</li>
+                    </ul>
+                </td>
+            </tr>
+            
+            <tr>
+                <td class="annee">Caractères Spéciaux</td>
+                <td class="intitule">
+                    <ul type = "square">
+                        <li> Curiosité intellectuelle</li>
+                        <li>passioné de challenge</li>
+                        <li>Dynamique</li>
+                        <li>sourire en toute circonstances</li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <a href="#haut"><center>Haut de page</center></a>
+
+    </div>
+
+
+    </body>
---
+++ src/php/Acceuil.php
@@ -0,0 +1,104 @@
+<?php
+?>
+
+<html>
+
+<head>
+
+<title>PORTFOLIO BTS SIO SLAM- Itic Paris</title>
+
+<link rel="stylesheet" type="text/css" href="../css/portfolio.css">
+
+</head>
+
+<body>
+
+
+
+	<div class="bigDiv">
+
+		<div class="header">
+			<img src="../Pictures/ProfilPicture.jpg" alt="photo de profil"
+				id="img">
+		</div>
+
+
+
+<!-- 		<div id="sommaire"> -->
+			<!-- 			  <h1 align="center"> PORTE FEUILLE DE COMPETENCES</h1> -->
+			<!-- 	 <ol type="I"> -->
+			<!-- 	<li> PPE 1ère année : Etude de l'intgration d'un nouvel outil -->
+			<!-- 			(cloud Computing à la M2L) -->
+			<!-- 	<li> Stage 1 ère année : Gestion d'incident (investigation, solution la mieux adaptée...) -->
+			<!-- 	<li> PPE 2 ème année : Gestion des places de parking à Rikola Entreprise -->
+			<!-- 	<li> Veille Technologique: LE CLOUD COMPUTING -->
+			<!-- 	</ol>  -->
+			<!-- 	<p> <b>PPE 1ère année :</b> Etude de l'intgration d'un nouvel outil -->
+			<!-- 			(cloud Computing à la M2L)</p> -->
+			<!-- 	<p> <b>Stage 1 ère année :</b> Gestion d'incident (investigation, solution la mieux adaptée...) -->
+			<!-- 	<p> <b>PPE 2 ème année :</b> Gestion des places de parking à Rikola Entreprise</p> -->
+			<!-- 	<p> <b>Veille Technologique:</b> LE CLOUD COMPUTING</p> -->
+
+<!-- 			<img src="../Pictures/portfolio.jpg" alt="logo portfolio" id="image"> -->
+<!-- 		</div> -->
+
+		<div class="header text-titre">
+			<p>
+				<b>Candidat:</b> <a href="curriculumVitae.php" target="_blank"> Momo
+					Kenfack Koko Jackeve </a>
+			</p>
+			<p>
+				<b>Diplôme:</b> <a href="http://www.bts-sio-formation.com/"
+					target="_blank">BTS SIO SLAM</a>
+			</p>
+			<p>
+				<b>Cursus:</b> <a
+					href="http://travail-emploi.gouv.fr/formation-professionnelle/formation-en-alternance/article/tout-comprendre-de-l-alternance"
+					target="_blank"> Alternance (Contrat de professionalisation) </a>
+			</p>
+			<p>
+				<b>Ecole:</b> <a
+					href="http://www.iticparis.com/?gclid=Cj0KEQiAr8W2BRD2qbCOv8_H7qEBEiQA1ErTBs1TJGtdBwI3-FeuvDWDSfZ4BQgm6UhiOgGygyJf-r0aAnbg8P8HAQ"
+					target="_blank">Itic Paris <img id="litleLogo" alt="logo Itic"
+					src="../Pictures/logoItic.jpg">
+				</a>
+			</p>
+			<p>
+				<b>Entreprise:</b> <a href="http://www.solvay.fr/fr/index.html"
+					target="_blank">Solvay <img id="litleLogo" alt="logo Solvay"
+					src="../Pictures/Solvay.png"></a>
+			</p>
+		</div>
+
+	</div>
+
+
+	<nav>
+		<div class="navigation">
+			<h3>SOMMAIRE</h3>
+			<ol type="I">
+				<li><b>PPE OBJET:</b> Inscriptions sportives M2L <a href=""><input
+						id="readMore" type="button" name="ppe1" value="lire la suite"></a>
+				
+				<li><b>PPE WEB :</b> Gestion des formations M2L <a href=""><input
+						id="readMore" type="button" name="ppe1" value="lire la suite"></a>
+				
+				<li><b>Veille Technologique:</b> LE CLOUD COMPUTING<a href=""><input
+						id="readMore" type="button" name="ppe1" value="lire la suite"></a>
+				
+				<li><b>Stage 1 ère année :</b> Support informatique<a href=""><input id="readMore"
+						type="button" name="ppe1" value="lire la suite"></a>
+			</ol>
+		</div>
+	</nav>
+
+
+	<div class="footer">
+		<a href="curriculumVitae.php"><span>Curriculum Vitae</span></a> <a
+			href=""><span>Cursus scolaire</span></a> <a href=""><span>Réseaux
+				sociaux</span></a> <a href=""><span>A Propos...</span></a>
+	</div>
+</body>
+
+
+</html>
