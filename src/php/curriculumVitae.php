1ab7c5f177ec931326c0ecc2d2981f9023f77101
kokojack <kokojackeve@gmail.com>
2016-03-02 01:05:17 +0100

    Initialisation du PortFolio

---
+++ src/php/curriculumVitae.php
@@ -0,0 +1,51 @@
+<?php
+?>
+
+<html>
+<head>
+<meta name="keywords"
+	content="cv, curriculum, vitae, compétences, suivi, cursus">
+<meta name="description" content="élaboration du cv">
+<link rel="stylesheet" type="text/css" href="../css/cv.css">
+<title>Curriculum Vitae</title>
+</head>
+
+<body>
+
+	<header>
+
+		<div class="bigDiv">
+
+			<div class="nav">
+				<a href="Acceuil.php"><span> Précedent</span></a> <a
+					href="Acceuil.php"><span>
+						<ul class="niv1">
+							<li>Sommaire
+								<ul class="En">
+									<li>OBJET</li>
+									<li>WEB</li>
+									<li>VEILLE</li>
+									<li>STAGE</li>
+									<li>Acceuil</li>
+								</ul>
+						
+						</ul>
+				</span></a> <a href="PPEOBJET.php"><span> Suivant</span></a>
+			</div>
+
+			<div class="categorie">
+				<h2>Catégorie : CV</h2>
+			</div>
+
+			<div class="cv" id="scrollbar">
+				<img src="../Pictures/ProfilPicture.jpg" alt="imgCv" class="scroll">
+			</div>
+
+		</div>
+
+	</header>
+
+</body>
+
+
+</html>
